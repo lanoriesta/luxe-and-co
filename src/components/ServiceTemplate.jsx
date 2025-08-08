1d830bca4d812a1ff5a2513557e9a5d1e0711a7b
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2025-08-09 06:18:33 +0800

    fix the template and add the data

--- src/components/ServiceTemplate.jsx
+++ src/components/ServiceTemplate.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import services from "../Services";
 
 const ServiceTemplate = () => {
-  return <div>ServiceTemplate</div>;
+  return (
+    <div>
+      {services.map((service, index) => (
+        <div key={index}>
+          <div>
+            <img src={service.img.src} alt={service.img.alt} />
+          </div>
+          <div>
+            <h2>{service.h2}</h2>
+            <h3>{service.subt}</h3>
+            <div>
+              {service.services.map((item, idx) => (
+                <div key={idx}>
+                  <h4>{item.title}</h4>
+                  <p>{item.desc}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ServiceTemplate;
