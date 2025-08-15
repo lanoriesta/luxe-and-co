6fbbbeff5f05afc84bc052b53bb22a3b5f569e66
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2025-08-15 20:57:10 +0800

    add CTASection component

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -4,19 +4,23 @@ import About from "./About";
 import SignatureServicesSection from "./SignatureServicesSection";
 import ServiceTemplate from "./ServiceTemplate";
 import TestimonialSection from "./TestimonialSection";
+import CTAsection from "./CTAsection";
 
 const MainBody = () => {
   return (
     <div>
       <MainKeyVisual />
-      <div className="bg-black h-[50px]"></div>
+      <div className="bg-black h-[100px]"></div>
       <About />
-      <div className="bg-black h-[50px]"></div>
+      <div className="bg-black h-[100px]"></div>
       <SignatureServicesSection />
       <div className="bg-black h-[100px]"></div>
       <ServiceTemplate />
       <div className="bg-black h-[100px]"></div>
       <TestimonialSection />
+      <div className="bg-black h-[100px]"></div>
+      <CTAsection />
+      <div className="bg-black h-[100px]"></div>
     </div>
   );
 };
